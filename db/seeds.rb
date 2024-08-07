5f9b7aae3e5dc6f1831e3d1d930c82570f88ee48
Ugo Bastiani <ugo.bastiani@hotmail.com>
2024-08-07 15:40:21 +0200

    exo 3 sans style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'cleaning db'
+Flat.destroy_all
+
+puts 'here is 5 new flats'
+
+appart1 = { name: 'Beatiful place', address: 'New York', description: 'Nice appart in NY', price_per_night: 90, number_of_guests: 4 }
+appart2 = { name: 'Nice House', address: 'Fontainebleau', description: 'Big house in Bleau', price_per_night: 180, number_of_guests: 12 }
+appart3 = { name: 'Very luxurous manor', address: 'Genève', description: 'A manor with Boo', price_per_night: 220, number_of_guests: 20 }
+appart4 = { name: 'Ancient castle', address: 'Munich', description: 'Winterfell', price_per_night: 2000, number_of_guests: 300 }
+appart5 = { name: 'The street Yo !', address: 'Los Angeles', description: 'Tiny house in the streets', price_per_night: 25, number_of_guests: 2 }
+
+[appart1, appart2, appart3, appart4, appart5].each do |attribute|
+  flat = Flat.create!(attribute)
+  puts "#{flat} created"
+end
+puts 'created !'
